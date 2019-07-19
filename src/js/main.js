e997fcd2d2dabc29bc681e7591d368db522e539b
Rafal Mroz <rafalmroz30@gmail.com>
2019-07-19 14:09:51 +0200

    feat: working js water counter

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -15,20 +15,40 @@ if ('serviceWorker' in navigator) {
 }
 
 // place your code below
-const key = new Date () .toISOString() .slice (0, 10);
+const key = new Date().toISOString().slice(0, 10);
 const counter = document.querySelector('.water__counter--js');
 
-const count = counter.innerHTML;
-if (localStorage.getItem(key,count)){
-  console.log(count);
-  counter.innerHTML = localStorage.getItem(key,count);
-localStorage.setItem(key, count);
-}else{
-  
-  localStorage.setItem(key, 0);
-  counter.innerHTML = 0;
-  
+var count = counter.innerHTML;
+counter.addEventListener('change', (e) => {
+
+})
+var checker = localStorage.getItem(key,count);
+console.log(checker);
+
+
+if (checker>0){
+  counter.innerHTML = checker;
+  count = checker;
 }
-console.log(count);
+
+////////////////BUTTONS///////////////////////////////////
+{
+  const plus = document.querySelector('.button__plus--js');
+plus.addEventListener('click', (e) => {                   
+  e.preventDefault();  
+                                    
+  localStorage.setItem(key, ++count);
+  counter.innerHTML = count;
+
+})
+const minus = document.querySelector('.button__minus--js');
+minus.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (count > 0) {
+    localStorage.setItem(key, --count);
+    counter.innerHTML = count;
+  }
+})
+}
 
 
